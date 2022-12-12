b8a32a12bf63fbbfaded924a531eb45d88a6e660
tuanvy <nguyentuanvy.a2.2018@gmail.com>
2022-12-12 20:35:46 +0700

    first commit

--- src/shared/axios.ts
+++ src/shared/axios.ts
@@ -4,7 +4,8 @@ import { API_URL } from "./constants";
 const instance = axios.create({
   baseURL: API_URL,
   params: {
-    api_key: process.env.REACT_APP_API_KEY,
+    api_key: "1af566f85ecb6a3b7bd189b25a385be8",
+    // api_key: process.env.REACT_APP_API_KEY,
   },
 });
 
